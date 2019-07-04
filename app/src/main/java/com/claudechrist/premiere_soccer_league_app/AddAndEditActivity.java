4668170a8038d17382468c9ba1cb5202256fec97
Claude Christ <christ.tchambila@gmail.com>
2019-07-04 14:55:08 +0200

    Add and edit basic functionality added

--- app/src/main/java/com/claudechrist/premiere_soccer_league_app/AddAndEditActivity.java
+++ app/src/main/java/com/claudechrist/premiere_soccer_league_app/AddAndEditActivity.java
@@ -1,16 +1,28 @@
 package com.claudechrist.premiere_soccer_league_app;
 
+import android.app.DatePickerDialog;
+import java.util.Calendar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Toast;
 
 import com.michaelmuenzer.android.scrollablennumberpicker.ScrollableNumberPicker;
 import com.michaelmuenzer.android.scrollablennumberpicker.ScrollableNumberPickerListener;
 
 public class AddAndEditActivity extends AppCompatActivity {
+
+    Calendar c;
+    DatePickerDialog datePickerDialog;
+    private Button pickDateBtn;
+    private int day;
+    private int month;
+    private int year;
 
 
     @Override
@@ -36,6 +48,7 @@ public class AddAndEditActivity extends AppCompatActivity {
     /**
      * Add the check menu icon to the tool bar
      * in AddAndEditActivity
+     *
      * @param menu
      * @return
      */
@@ -45,6 +58,39 @@ public class AddAndEditActivity extends AppCompatActivity {
         inflater.inflate(R.menu.add_menu, menu);
 
         return true;
+    }
+
+    public void pickDate(View view) {
+        c = Calendar.getInstance();
+
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int month = c.get(Calendar.MONTH) + 1;
+        int year = c.get(Calendar.YEAR);
+
+        datePickerDialog = new DatePickerDialog(AddAndEditActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                setDay(dayOfMonth);
+                setMonth(month + 1);
+                setYear(year + 1);
+            }
+        }, day, month, year);
+
+
+        datePickerDialog.show();
+
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 
     /*// this method adds the menu to the toolbar
