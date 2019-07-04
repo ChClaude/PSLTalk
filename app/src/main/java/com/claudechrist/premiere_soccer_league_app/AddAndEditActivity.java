f82247183914588c34dda3d035a0aedaf8a0092e
Claude Christ <christ.tchambila@gmail.com>
2019-07-04 13:04:50 +0200

    number picker added to the interface

--- app/src/main/java/com/claudechrist/premiere_soccer_league_app/AddAndEditActivity.java
+++ app/src/main/java/com/claudechrist/premiere_soccer_league_app/AddAndEditActivity.java
@@ -1,13 +1,80 @@
 package com.claudechrist.premiere_soccer_league_app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.michaelmuenzer.android.scrollablennumberpicker.ScrollableNumberPicker;
+import com.michaelmuenzer.android.scrollablennumberpicker.ScrollableNumberPickerListener;
+
+public class AddAndEditActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.add_and_edit);
+
+        Toolbar toolbar = findViewById(R.id.AddToolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_close);
+
+        final ScrollableNumberPicker verticalNumberPicker = (ScrollableNumberPicker) findViewById(R.id.number_picker_vertical_icons);
+
+        verticalNumberPicker.setListener(new ScrollableNumberPickerListener() {
+            @Override
+            public void onNumberPicked(int value) {
+                Toast.makeText(AddAndEditActivity.this, "number picker picked at value " + value, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    /**
+     * Add the check menu icon to the tool bar
+     * in AddAndEditActivity
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_menu, menu);
+
+        return true;
+    }
+
+    /*// this method adds the menu to the toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    // here we set a listener to the item created in the menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.search_match:
+                Toast.makeText(this, "Search match Item clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.delete_match:
+                Toast.makeText(this, "Delete match Item clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.new_match:
+                Toast.makeText(this, "New match Item clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }*/
+
 }
