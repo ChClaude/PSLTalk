c87544591491b82b3f5357a4a3b8ea45437518c0
Claude Christ <christ.tchambila@gmail.com>
2019-07-05 14:14:22 +0200

    main activity toolbar added

--- app/src/main/java/com/claudechrist/premiere_soccer_league_app/MainActivity.java
+++ app/src/main/java/com/claudechrist/premiere_soccer_league_app/MainActivity.java
@@ -1,11 +1,16 @@
 package com.claudechrist.premiere_soccer_league_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,10 +25,40 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, AddAndEditActivity.class);
+                startActivity(intent);
             }
         });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.search_match:
+                Toast.makeText(this, "search icon clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.app_settings:
+                Toast.makeText(this, "settings clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.new_match:
+                Toast.makeText(this, "new match clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.delete_match:
+                Toast.makeText(this, "delete match clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
 }
