1de69888ef1abe580ce4eb25ce77e08d09ad38f6
Claude Christ <christ.tchambila@gmail.com>
2019-07-06 10:40:07 +0200

    Database functionality added

--- app/src/main/java/com/claudechrist/premiere_soccer_league_app/MainActivity.java
+++ app/src/main/java/com/claudechrist/premiere_soccer_league_app/MainActivity.java
@@ -1,11 +1,14 @@
 package com.claudechrist.premiere_soccer_league_app;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -29,6 +32,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        try {
+            SQLiteDatabase soccerEventSDatabase = this.openOrCreateDatabase("Soccer Events", MODE_PRIVATE, null);
+            soccerEventSDatabase.execSQL("CREATE TABLE IF NOT EXISTS 'match' (id INT PRIMARY KEY, teamA VARCHAR, sccoreA INT, teamB VARCHAR, sccoreB INT, label VARCHAR, date VARCHAR)");
+
+            Cursor c = soccerEventSDatabase.rawQuery("SELECT * FROM 'match'", null);
+            int idIndex = c.getColumnIndex("id");
+            int teamAIndex = c.getColumnIndex("teamA");
+            int teamBIndex = c.getColumnIndex("teamB");
+            int dateIndex = c.getColumnIndex("date");
+
+            Log.i("ID index", Integer.toString(idIndex));
+            Log.i("Team A index", Integer.toString(teamAIndex));
+            Log.i("Team B index", Integer.toString(teamBIndex));
+            Log.i("Date index", Integer.toString(dateIndex));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
