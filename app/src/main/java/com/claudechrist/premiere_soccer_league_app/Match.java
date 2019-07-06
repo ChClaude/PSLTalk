936f6ccb90c657f6ba7ac0dddd93074f2a89e9d6
Claude Christ <christ.tchambila@gmail.com>
2019-07-06 12:06:25 +0200

    Match class added

---
+++ app/src/main/java/com/claudechrist/premiere_soccer_league_app/Match.java
@@ -0,0 +1,79 @@
+package com.claudechrist.premiere_soccer_league_app;
+
+public class Match {
+
+    private String teamA;
+    private String teamB;
+    private int scoreA;
+    private int scoreB;
+    private String label;
+    private String date;
+
+    public Match(String teamA, String teamB, int scoreA, int scoreB, String label, String date) {
+        this.teamA = teamA;
+        this.teamB = teamB;
+        this.scoreA = scoreA;
+        this.scoreB = scoreB;
+        this.label = label;
+        this.date = date;
+    }
+
+    public String getTeamA() {
+        return teamA;
+    }
+
+    public void setTeamA(String teamA) {
+        this.teamA = teamA;
+    }
+
+    public String getTeamB() {
+        return teamB;
+    }
+
+    public void setTeamB(String teamB) {
+        this.teamB = teamB;
+    }
+
+    public int getScoreA() {
+        return scoreA;
+    }
+
+    public void setScoreA(int scoreA) {
+        this.scoreA = scoreA;
+    }
+
+    public int getScoreB() {
+        return scoreB;
+    }
+
+    public void setScoreB(int scoreB) {
+        this.scoreB = scoreB;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Match{"  +
+                "Team A: " + teamA + " Score A: " + scoreA + "\n" +
+                "Team B: " + teamB + " Score B: " + scoreB + "\n" +
+                "Label " + label + " Date " + date +
+                '}';
+    }
+
+}
